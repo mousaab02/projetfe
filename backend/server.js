c5395991417bf8ea7211fde447462ee06489d943
mousaab02 <mossaab.laachir@gmail.com>
2025-06-18 01:00:31 +0100

    Ajout Jenkinsfile et modifications backend

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const formulaireRoutes = require('./routes/formulaireRoutes');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// MongoDB connection
+mongoose.connect('mongodb://127.0.0.1:27017/formulairedb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connecté'))
+  .catch(err => console.error(err));
+
+// Routes
+app.use('/api/formulaire', formulaireRoutes);
+
+// Lancement du serveur
+app.listen(PORT, () => {
+    console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
